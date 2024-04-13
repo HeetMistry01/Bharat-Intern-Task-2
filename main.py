232ce46e8df5077c7ab53853f3137f885d78dd5d
HeetMistry01 <166299678+HeetMistry01@users.noreply.github.com>
2024-04-13 13:43:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data=pd.read_csv('/Users/heetmistry/Desktop/Movie Reccomend/top10K-TMDB-movies.csv')
+
+print(data)
+
+#to run html command:' streamlit run app.py 'in terminal 
